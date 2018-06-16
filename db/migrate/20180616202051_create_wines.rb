468df1af8b70015353d15298c2288c2120d78cbb
Raghu Betina <raghu@firstdraft.com>
2018-06-16 20:20:51 +0000

    Generate wines resource with starter:resource

---
+++ db/migrate/20180616202051_create_wines.rb
@@ -0,0 +1,15 @@
+class CreateWines < ActiveRecord::Migration
+  def change
+    create_table :wines do |t|
+      t.integer :sweetness
+      t.string :name
+      t.string :category
+      t.string :body
+      t.string :variety
+      t.string :origin
+
+      t.timestamps
+
+    end
+  end
+end
