00cfca3796657394c27292e29a4e0ada51f23af2
Raghu Betina <raghu@firstdraft.com>
2018-06-16 23:30:16 +0000

    3rd draft

--- app/admin/wines.rb
+++ app/admin/wines.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Wine do
 
- permit_params :sweetness, :name, :category, :body, :variety, :origin
+ permit_params :sweetness, :name, :category, :body, :variety, :origin, :image
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
