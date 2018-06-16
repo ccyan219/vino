f76aae108f09f44f43852525cf4d1e569e8986c1
Raghu Betina <raghu@firstdraft.com>
2018-06-16 20:21:11 +0000

    Direct association between locations and listings

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,10 @@
 class Location < ApplicationRecord
   # Direct associations
 
+  has_many   :catalogs,
+             :class_name => "Listing",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
