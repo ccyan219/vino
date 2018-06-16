507dcc847dca0e75f6eed753fb309ab43d092c74
Raghu Betina <raghu@firstdraft.com>
2018-06-16 20:21:28 +0000

    Whitelist columns in Wine for ActiveAdmin

--- app/admin/wines.rb
+++ app/admin/wines.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Wine do
+
+ permit_params :sweetness, :name, :category, :body, :variety, :origin
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
